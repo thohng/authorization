ed0dd87700b41b5403cba519e8fe1d92bb01daae
Do Trong Long <long@get4x.co>
2015-12-03 11:27:19 +0700

    Add methods to get roles

--- src/Contracts/Role/HasRoles.php
+++ src/Contracts/Role/HasRoles.php
@@ -1,9 +1,23 @@
 <?php namespace TechExim\Auth\Contracts\Role;
 
+use TechExim\Auth\Contracts\Role;
+
 interface HasRoles
 {
     /**
      * @return mixed
      */
     public function getRoles();
+
+    /**
+     * @param Role $role
+     * @return bool
+     */
+    public function hasRole(Role $role);
+
+    /**
+     * @param $name
+     * @return bool
+     */
+    public function hasRoleByName($name);
 }
--- src/Role/HasRoles.php
+++ src/Role/HasRoles.php
@@ -4,8 +4,37 @@ use TechExim\Auth\Contracts\Role\Repository as RoleRepository;
 
 trait HasRoles
 {
+    /**
+     * @return RoleRepository
+     */
+    protected function getRoleRepository()
+    {
+        return app(RoleRepository::class);
+    }
+
     public function getRoles()
     {
+        return $this->getRoleRepository()->getObjectRoles($this);
+    }
 
+    public function hasRole(Role $role)
+    {
+        foreach ($this->getRoles() as $objectRole) {
+            if ($objectRole instanceof Role && $objectRole->getId() === $role->getId()) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public function hasRoleByName($name)
+    {
+        $role = $this->getRoleRepository()->getRole($name);
+        if ($role) {
+            return $this->hasRole($role);
+        }
+
+        return false;
     }
 }
