0fa9195404cafbb3a5384f5d1af086bdbd8fe90f
Long Do <me@dotronglong.com>
2015-12-08 16:35:43 +0700

    Allow to detect multiple roles

--- src/Role/HasRoles.php
+++ src/Role/HasRoles.php
@@ -1,5 +1,6 @@
 <?php namespace TechExim\Auth\Role;
 
+use Illuminate\Database\Eloquent\Collection;
 use TechExim\Auth\Contracts\Role\Repository as RoleRepository;
 use TechExim\Auth\Contracts\Role;
 
@@ -13,11 +14,18 @@ trait HasRoles
         return app(RoleRepository::class);
     }
 
+    /**
+     * @return Collection
+     */
     public function getRoles()
     {
         return $this->getRoleRepository()->getObjectRoles($this);
     }
 
+    /**
+     * @param Role $role
+     * @return bool
+     */
     public function hasRole(Role $role)
     {
         foreach ($this->getRoles() as $objectRole) {
@@ -29,6 +37,10 @@ trait HasRoles
         return false;
     }
 
+    /**
+     * @param $name
+     * @return bool
+     */
     public function hasRoleByName($name)
     {
         $role = $this->getRoleRepository()->getRole($name);
@@ -37,5 +49,29 @@ trait HasRoles
         }
 
         return false;
+    }
+
+    /**
+     * @param Collection $roles
+     * @return bool
+     */
+    public function hasRoles(Collection $roles)
+    {
+        foreach ($roles as $role) {
+            if ($role instanceof Role && $this->hasRole($role)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * @param array $names
+     * @return bool
+     */
+    public function hasRolesByName(array $names)
+    {
+        return $this->hasRoles($this->getRoleRepository()->getRoles($names));
     }
 }
